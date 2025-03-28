3a5cf018191c0f7da9050dac8a8f3096423e3fb1
lucaskraus <lucas-kraus@hotmail.com>
2025-03-28 18:14:15 -0300

    Cleaning project

--- apps/expo-app/app/_layout.tsx
+++ apps/expo-app/app/_layout.tsx
@@ -5,17 +5,13 @@ import {
   ThemeProvider,
 } from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+//@ts-ignore
+import { Stack, ErrorBoundary } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { useColorScheme } from "@/components/useColorScheme";
 import "../global.css";
-
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from "expo-router";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
@@ -56,12 +52,6 @@ function RootLayoutNav() {
     <GluestackUIProvider mode={(colorScheme ?? "light") as "light" | "dark"}>
       <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
         <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="signin" />
-          <Stack.Screen name="signup" />
-          <Stack.Screen name="forgot-password" />
-          <Stack.Screen name="create-password" />
-          <Stack.Screen name="news-feed" />
-          <Stack.Screen name="dashboard" />
           <Stack.Screen name="profile" />
         </Stack>
       </ThemeProvider>
--- apps/expo-app/app/index.tsx
+++ apps/expo-app/app/index.tsx
@@ -9,56 +9,6 @@ const index = () => {
       <VStack className="p-2 md:max-w-[440px] w-full" space="xl">
         <Button
           onPress={() => {
-            router.push("auth/splash-screen");
-          }}
-        >
-          <ButtonText>SplashScreen</ButtonText>
-        </Button>
-        <Button
-          className="w-full"
-          onPress={() => {
-            router.push("auth/signin");
-          }}
-        >
-          <ButtonText>Sign in</ButtonText>
-        </Button>
-        <Button
-          onPress={() => {
-            router.push("auth/signup");
-          }}
-        >
-          <ButtonText>Sign up</ButtonText>
-        </Button>
-        <Button
-          onPress={() => {
-            router.push("auth/forgot-password");
-          }}
-        >
-          <ButtonText>Forgot password</ButtonText>
-        </Button>
-        <Button
-          onPress={() => {
-            router.push("auth/create-password");
-          }}
-        >
-          <ButtonText>Create password</ButtonText>
-        </Button>
-        <Button
-          onPress={() => {
-            router.push("news-feed/news-and-feed");
-          }}
-        >
-          <ButtonText>News feed</ButtonText>
-        </Button>
-        <Button
-          onPress={() => {
-            router.push("dashboard/dashboard-layout");
-          }}
-        >
-          <ButtonText>Dashboard</ButtonText>
-        </Button>
-        <Button
-          onPress={() => {
             router.push("profile/profile");
           }}
         >
